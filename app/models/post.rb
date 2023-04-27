1c941b814b39d1911a14170f33b9fa1515924c1a
Kate Bennert <kate.bennert@gmail.com>
2023-04-27 16:56:54 -0400

    commit tree 6a2c9dee40f52da620b9bc2c652dfcf268a48763

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        if title
+            unless title.match?(/Won't Believe|Secret|Top \d|Guess/)
+                errors.add(:title, "Needs more bait.")
+            end
+        end
+    end
 end
